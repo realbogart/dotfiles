597ddc336591181bfe9d53c83da42dd2cad8516d
Johan Yngman <johan.yngman@paradoxinteractive.com>
2023-09-05 14:43:18 +0200

    Install starship if needed

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+[[ $(type starship) ]] && echo "starship already installed."  || { curl -sS https://starship.rs/install.sh | sh; }
+
 ln -s $SCRIPTPATH/.bashrc ~/.bashrc
 ln -s $SCRIPTPATH/.zshrc ~/.zshrc
 ln -s $SCRIPTPATH/nvim ~/.config
