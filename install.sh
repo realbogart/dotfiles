ed567ed7f276050ff9c42b18a9bff2b91b873578
Johan Yngman <johan.yngman@paradoxinteractive.com>
2023-09-05 13:44:48 +0200

    Added .zshrc

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 ln -s $SCRIPTPATH/.bashrc ~/.bashrc
+ln -s $SCRIPTPATH/.zshrc ~/.zshrc
 ln -s $SCRIPTPATH/nvim ~/.config
+ln -s $SCRIPTPATH/tmux ~/.config
 
