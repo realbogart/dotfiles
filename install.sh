dc8c11ea4b411800c91d89c0dd3e240559a224e9
Johan Yngman <johan.yngman@paradoxinteractive.com>
2023-09-05 14:48:06 +0200

    Added installation of oh-my-zsh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+[[ -d ~/.oh-my-zsh ]] && echo "oh-my-zsh already installed."  || { sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; }
 [[ $(type starship) ]] && echo "starship already installed."  || { curl -sS https://starship.rs/install.sh | sh; }
 
 ln -s $SCRIPTPATH/.bashrc ~/.bashrc
