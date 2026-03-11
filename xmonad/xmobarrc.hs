feaa905e5d9ee538c22ec5d9f69a938b39f8e393
Johan Yngman <johan.yngman@gmail.com>
2026-03-11 20:28:49 +0100

    Colors

--- xmonad/xmobarrc.hs
+++ xmonad/xmobarrc.hs
@@ -2,16 +2,17 @@ Config
   { font = "FiraMono Nerd Font Mono 14"
   , additionalFonts = []
   , dpi = 120
-  , bgColor = "#1f2430"
-  , fgColor = "#d8dee9"
+  , bgColor = "#282828"
+  , fgColor = "#b3deef"
   , position = BottomSize C 100 34
   , lowerOnStart = True
   , allDesktops = True
   , persistent = True
   , commands =
-      [ Run Date "%Y-%m-%d %H:%M" "date" 14
+      [ Run Date "%Y-%m-%d" "date" 600
+      , Run Date "%H:%M" "time" 14
       ]
   , sepChar = "%"
   , alignSep = "}{"
-  , template = "}{ %date% "
+  , template = "}{ <fc=#d3b987>%date%</fc> %time% "
   }
