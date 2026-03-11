90b7eaa97281f5a7fb0a3b2b9401a40cd47f2efc
Johan Yngman <johan.yngman@gmail.com>
2026-03-11 20:46:11 +0100

    Color twea

--- xmonad/xmobarrc.hs
+++ xmonad/xmobarrc.hs
@@ -1,18 +1,18 @@
 Config
-  { font = "FiraMono Nerd Font Mono 14"
+  { font = "FiraMono Nerd Font Mono 12"
   , additionalFonts = []
   , dpi = 120
   , bgColor = "#282828"
   , fgColor = "#b3deef"
-  , position = BottomSize C 100 34
+  , position = BottomSize C 100 32
   , lowerOnStart = True
   , allDesktops = True
   , persistent = True
   , commands =
       [ Run Date "%Y-%m-%d" "date" 600
-      , Run Date "%H:%M" "time" 14
+      , Run Date "%H:%M" "time" 10
       ]
   , sepChar = "%"
   , alignSep = "}{"
-  , template = "}{ <fc=#d3b987>%date%</fc> %time% "
+  , template = "}{ <fc=#d3b987>%date%</fc> <fc=#eeeeee>%time%</fc> "
   }
