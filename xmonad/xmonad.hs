70b7bbf1d1ed9d91e645a620284deb0d070627cd
Johan Yngman <johan.yngman@gmail.com>
2026-03-11 22:13:09 +0100

    More keybindings

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -58,18 +58,16 @@ customKeys c =
   , ((modm, xK_j), windows $ W.greedyView "4")
   , ((modm, xK_k), windows $ W.greedyView "5")
   , ((modm, xK_l), windows $ W.greedyView "6")
-  , ((modm, xK_u), windows $ W.greedyView "7")
+  , ((modm, xK_u), sendMessage Shrink)
   , ((modm, xK_i), goToSelected def)
-  , ((modm, xK_o), windows $ W.greedyView "9")
+  , ((modm, xK_o), sendMessage Expand)
+  , ((modm, xK_n), kill)
   , ((modm, xK_1), windows $ W.shift "1")
   , ((modm, xK_2), windows $ W.shift "2")
   , ((modm, xK_3), windows $ W.shift "3")
   , ((modm, xK_4), windows $ W.shift "4")
   , ((modm, xK_5), windows $ W.shift "5")
   , ((modm, xK_6), windows $ W.shift "6")
-  , ((modm, xK_7), windows $ W.shift "7")
-  , ((modm, xK_8), windows $ W.shift "8")
-  , ((modm, xK_9), windows $ W.shift "9")
   ]
   where
     modm = modMask c
